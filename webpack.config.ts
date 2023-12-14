2aa9f0a068b66ac802ef99af66af390900482b6d
Ilia Nikolaev <iliateletski@mail.com>
2023-12-14 12:19:37 +0700

    Add project assembly

---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import webpack from 'webpack'
+import { buildWebpack } from './config/build/buildWebpack'
+import { BuildMode, BuildPaths } from './config/build/types/types'
+
+interface EnvVariables {
+	mode: BuildMode
+	port: number
+}
+
+export default (env: EnvVariables) => {
+	const isDev = env.mode === 'development'
+
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, 'src', 'index.tsx'),
+		output: path.resolve(__dirname, 'build')
+	}
+
+	const config: webpack.Configuration = buildWebpack({
+		mode: env.mode ?? 'development',
+		port: env.port ?? 3000,
+		paths
+	})
+	return config
+}
